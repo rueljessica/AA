4b52d414bc66fbb49d92ae6874083f518bf57433
jess <39679553+rueljessica@users.noreply.github.com>
2019-07-04 21:58:51 -0300

    Add files via upload

---
+++ src/aacomp/FabricaInstalacao.java
@@ -0,0 +1,7 @@
+package aacomp;
+
+public class FabricaInstalacao {
+	public void criarInstalacao(double temperatura, int capacidadeMaxima, double altura, double largura, double comprimento) {
+		
+	}
+}
