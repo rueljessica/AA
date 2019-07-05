d668a8c4ec91c86d6307e663443748fc18c76ce1
jess <39679553+rueljessica@users.noreply.github.com>
2019-07-04 21:54:44 -0300

    Update Anfibio.java

--- src/aacomp/Anfibio.java
+++ src/aacomp/Anfibio.java
@@ -1,5 +1,10 @@
 package aacomp;
 
 public interface Anfibio extends Terrestre, Aquatico {
-	
+	default void nadar() {
+		System.out.println("estou nadando!");
+	}
+	default void andar() {
+		System.out.println("estou andando!");
+	}	
 }
