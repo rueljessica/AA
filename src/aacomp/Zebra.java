6bb9af95fd24a716bf7469f8a243623c5ba17f33
jess <39679553+rueljessica@users.noreply.github.com>
2019-07-08 22:42:06 -0300

    Update Zebra.java

--- src/aacomp/Zebra.java
+++ src/aacomp/Zebra.java
@@ -6,6 +6,7 @@ public class Zebra implements Terrestre{
 	private Porte porte;
 	private Sexo sexo;
 	Alimento[] alimento = {Alimento.FRUTAS, Alimento.VERDURAS}; // definindo o tipo de alimento que este animal come
+	
 	@Override
 	public boolean alimentar(Alimento alimento) {
 		// comparando o tipo de alimento recebido com o tipo que o animal come 
@@ -16,18 +17,22 @@ public class Zebra implements Terrestre{
 		}
 		return false;
 	}
+	
 	@Override
 	public Sexo getSexo() {
 		return this.sexo;
 	}
+	
 	@Override
 	public void setSexo(Sexo sexo) {
 		this.sexo = sexo;
 	}
+	
 	@Override
 	public Porte getPorte() {
 		return this.porte;
 	}
+	
 	@Override
 	public void setPorte(Porte porte) {
 		this.porte = porte;
