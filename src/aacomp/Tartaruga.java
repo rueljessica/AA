5fa94b63c87a69426af906e57297e23fcb9b6cde
jess <39679553+rueljessica@users.noreply.github.com>
2019-07-08 22:41:19 -0300

    Update Tartaruga.java

--- src/aacomp/Tartaruga.java
+++ src/aacomp/Tartaruga.java
@@ -4,6 +4,7 @@ public class Tartaruga implements Anfibio{
 	private Porte porte;
 	private Sexo sexo;
 	Alimento[] alimento = {Alimento.FRUTAS, Alimento.VERDURAS, Alimento.PEIXES}; // definindo o tipo de alimento que este animal come
+	
 	@Override
 	public boolean alimentar(Alimento alimento) {
 		// comparando o tipo de alimento recebido com o tipo que o animal come 
@@ -14,18 +15,22 @@ public class Tartaruga implements Anfibio{
 		}
 		return false;
 	}
+	
 	@Override
 	public Sexo getSexo() {
 		return this.sexo;
 	}
+	
 	@Override
 	public void setSexo(Sexo sexo) {
 		this.sexo = sexo;
 	}
+	
 	@Override
 	public Porte getPorte() {
 		return this.porte;
 	}
+	
 	@Override
 	public void setPorte(Porte porte) {
 		this.porte= porte;
