074d80b10363f1c3618c2ec1efca15a6cd7ad71e
jess <39679553+rueljessica@users.noreply.github.com>
2019-07-08 21:26:04 -0300

    Update ElementoInstalacao.java

--- src/aacomp/ElementoInstalacao.java
+++ src/aacomp/ElementoInstalacao.java
@@ -1,7 +1,7 @@
 package aacomp;
 
 public abstract class ElementoInstalacao extends Instalacao {
-
+	//construtor com os atributos da superclasse
 	public ElementoInstalacao(int capacidadeMaxima, Localizacao localizacao, double temperatura, Animal[] animaisInstalados) {
 		super(capacidadeMaxima, localizacao, temperatura, animaisInstalados);
 	}
