60bcc088f8622feb519d7990546728f8a58a71df
jess <39679553+rueljessica@users.noreply.github.com>
2019-07-08 22:41:46 -0300

    Update Tubarao.java

--- src/aacomp/Tubarao.java
+++ src/aacomp/Tubarao.java
@@ -4,6 +4,7 @@ public class Tubarao implements Aquatico {
 	private Porte porte;
 	private Sexo sexo;
 	Alimento[] alimento = {Alimento.CARNES, Alimento.PEIXES}; // definindo o tipo de alimento que este animal come
+	
 	@Override
 	public boolean alimentar(Alimento alimento) {
 		// comparando o tipo de alimento recebido com o tipo que o animal come 
@@ -14,14 +15,17 @@ public class Tubarao implements Aquatico {
 		}
 		return false;
 	}
+	
 	@Override
 	public Sexo getSexo() {
 		return this.sexo;
 	}
+	
 	@Override
 	public void setSexo(Sexo sexo) {
 		this.sexo = sexo;
 	}
+	
 	@Override
 	public Porte getPorte() {
 		return this.porte;
