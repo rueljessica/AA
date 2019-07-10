0c8fd3d3126766a180201b1371053c09f4529eba
jess <39679553+rueljessica@users.noreply.github.com>
2019-07-09 23:36:14 -0300

    Update Jaula.java

--- src/aacomp/Jaula.java
+++ src/aacomp/Jaula.java
@@ -2,16 +2,19 @@ package aacomp;
 
 public class Jaula extends ElementoInstalacao {
 	private double altura, largura, comprimento;
-	public Jaula(int capacidadeMaxima, Localizacao localizacao, double temperatura, Animal[] animaisInstalados, double altura, double largura, double comprimento) {
+	public Jaula(int capacidadeMaxima, Localizacao localizacao, double temperatura, Animal[] animaisInstalados,
+			double altura, double largura, double comprimento) {
 		super(capacidadeMaxima, localizacao, temperatura, animaisInstalados);
 		this.altura = altura;
 		this.largura = largura;
 		this.comprimento = comprimento;
 	}
 	public double area() {
-		//calculando area da jaula
-		double area = 2*((this.altura*this.comprimento)+(this.altura*this.largura)+(this.comprimento*this.largura));
+		double area = 2 *((this.altura * this.comprimento ) + (this.altura * this.largura)+(this.comprimento * this.largura));
 		return area;
+	}
+	public Jaula() {
+		super();
 	}
 	public double getAltura() {
 		return altura;
