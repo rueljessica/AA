f232b5c0d7e49618a3b5938526f8805b1d6e0356
jess <39679553+rueljessica@users.noreply.github.com>
2019-07-09 23:34:04 -0300

    Update FabricaInstalacao.java

--- src/aacomp/FabricaInstalacao.java
+++ src/aacomp/FabricaInstalacao.java
@@ -4,162 +4,243 @@ public class FabricaInstalacao {
 	/* em cada método p/ criar a instalaçao de cada animal foi feito: 
 		a contagem dos animais no vetor e foi comparado com a capacidade maxima da instalação 
 		quando ultrapassa o limite, é apresentada uma mensagem 
-		quando a quantidade de animais é menor ou igual ao limite, é criada a instalação 
-		para as jaulas, é imprimido a area total da jaula criada
+		quando a quantidade de animais é menor ou igual ao limite, é criada a instalação
 		e se for menor que o limite, é apresentada uma mensagem falando quantos animais ainda podem ser adicionados à instalação
 	*/
 	@SuppressWarnings("unused")
 	public static void criarInstalacaoElefante(Localizacao localizacao, Animal[] animal) {
-		int cont = 0 ;
-		for(Animal a : animal) {
-			cont++;
-		}
-		if(cont<=2) {
-			Jaula jaulaElefante = new Jaula(2, localizacao, 23, animal, 20, 30, 25);
-			System.out.println("A AREA DESSA JAULA É: "+ jaulaElefante.area());
-			if(cont<2) {
-				System.out.println("VOCE PODE ADICIONAR MAIS "+(2-cont)+" ELEFANTE À ESTA JAULA.");
+		Jaula jaulaElefante = new Jaula();
+		jaulaElefante.setCapacidadeMaxima(2);
+		jaulaElefante.setLocalizacao(localizacao);
+		jaulaElefante.setTemperatura(23);
+		jaulaElefante.setAltura(20);
+		jaulaElefante.setLargura(30);
+		jaulaElefante.setComprimento(25);
+		
+		System.out.println("A AREA DESSA JAULA É: "+ jaulaElefante.area());
+		int i=0;
+		
+		for(i=0;i<animal.length;i++) {
+			jaulaElefante.instalaAnimal(animal[i], i);
+			boolean b = jaulaElefante.instalaAnimal(animal[i], i);
+			if(b) {
+				System.out.println("ELEFANTE "+ (i+1) +" INSTALADO COM SUCESSO");
 			}
-		}else {
-			System.out.println("QUANTIDADE DE ANIMAIS ULTRAPASSA A CAPACIDADE MAXIMA");
 		}
+		if(i<2) {
+			System.out.println("VOCE PODE ADICIONAR MAIS "+(2-i)+" ELEFANTE À ESTA JAULA.");
+		}else {
+			System.out.println("CAPACIDADE MAXIMA ATINGIDA");
+		}
+		System.out.println("----------------------------------------------------------------------");
 	}
 	@SuppressWarnings("unused")
 	public static void criarInstalacaoMacaco(Localizacao localizacao, Animal[] animal) {
-		int cont = 0 ;
-		for(Animal a : animal) {
-			cont++;
-		}
-		if(cont<10) {
-			Jaula jaulaMacaco = new Jaula(10, localizacao, 25, animal, 40, 35.7, 90);
-			System.out.println("A AREA DESSA JAULA É: "+ jaulaMacaco.area());
-			if(cont<10) {
-				System.out.println("VOCE PODE ADICIONAR MAIS "+(10-cont)+" MACACO(S) À ESTA JAULA.");
+		Jaula jaulaMacaco = new Jaula();
+		jaulaMacaco.setCapacidadeMaxima(10);
+		jaulaMacaco.setLocalizacao(localizacao);
+		jaulaMacaco.setTemperatura(25);
+		jaulaMacaco.setAltura(40);
+		jaulaMacaco.setLargura(35.7);
+		jaulaMacaco.setComprimento(90);
+		
+		System.out.println("A AREA DESSA JAULA É: "+ jaulaMacaco.area());
+		int i=0;
+		
+		for(i=0;i<animal.length;i++) {
+			boolean b = jaulaMacaco.instalaAnimal(animal[i], i);
+			if(b) {
+				System.out.println("MACACO "+(i+1) +" INSTALADO COM SUCESSO");
 			}
-		}else {
-			System.out.println("QUANTIDADE DE ANIMAIS ULTRAPASSA A CAPACIDADE MAXIMA");
 		}
+		if(i<10) {
+			System.out.println("VOCE PODE ADICIONAR MAIS "+(10-i)+" MACACO(S) À ESTA JAULA.");
+		}else{
+			System.out.println("CAPACIDADE MAXIMA ATINGIDA");
+		}
+		System.out.println("----------------------------------------------------------------------");
 	}
 	@SuppressWarnings("unused")
 	public static void criarInstalacaoZebra(Localizacao localizacao, Animal[] animal) {
-		int cont = 0 ;
-		for(Animal a : animal) {
-			cont++;
-		}
-		if(cont<=2) {
-			Jaula jaulaZebra = new Jaula(2, localizacao, 24, animal, 20, 30, 25);
-			System.out.println("A AREA DESSA JAULA É: "+ jaulaZebra.area());
-			if(cont<2) {
-				System.out.println("VOCE PODE ADICIONAR MAIS "+(2-cont)+" ZEBRA À ESTA JAULA.");
+		Jaula jaulaZebra = new Jaula();
+		jaulaZebra.setCapacidadeMaxima(2);
+		jaulaZebra.setLocalizacao(localizacao);
+		jaulaZebra.setTemperatura(24);
+		jaulaZebra.setAltura(20);
+		jaulaZebra.setLargura(30);
+		jaulaZebra.setComprimento(25);
+		
+		System.out.println("A AREA DESSA JAULA É: "+ jaulaZebra.area());
+		int i=0;
+		
+		for(i=0;i<animal.length;i++) {
+			boolean b = jaulaZebra.instalaAnimal(animal[i], i);
+			if(b) {
+				System.out.println("ZEBRA "+(i+1) +" INSTALADO COM SUCESSO");
 			}
-		}else {
-			System.out.println("QUANTIDADE DE ANIMAIS ULTRAPASSA A CAPACIDADE MAXIMA");
 		}
+		if(i<2) {
+			System.out.println("VOCE PODE ADICIONAR MAIS "+(2-i)+" ZEBRA À ESTA JAULA.");
+		}else{
+			System.out.println("CAPACIDADE MAXIMA ATINGIDA");
+		}
+		System.out.println("----------------------------------------------------------------------");
 	}
 	@SuppressWarnings("unused")
 	public static void criarInstalacaoLeao(Localizacao localizacao, Animal[] animal) {
-		int cont = 0 ;
-		for(Animal a : animal) {
-			cont++;
-		}
-		if(cont<=3) {
-			Jaula jaulaLeao = new Jaula(3, localizacao, 21.5, animal, 20, 30, 25);
-			System.out.println("A AREA DESSA JAULA É: "+ jaulaLeao.area());
-			if(cont < 3) {
-				System.out.println("VOCE PODE ADICIONAR MAIS "+(3-cont)+" LEAO(ES) À ESTA JAULA.");
+		Jaula jaulaLeao = new Jaula();
+		jaulaLeao.setCapacidadeMaxima(3);
+		jaulaLeao.setLocalizacao(localizacao);
+		jaulaLeao.setTemperatura(21.5);
+		jaulaLeao.setAltura(20);
+		jaulaLeao.setLargura(30);
+		jaulaLeao.setComprimento(25);
+		
+		System.out.println("A AREA DESSA JAULA É: "+ jaulaLeao.area());
+		int i=0;
+		
+		for(i=0;i<animal.length;i++) {
+			boolean b = jaulaLeao.instalaAnimal(animal[i], i);
+			if(b) {
+				System.out.println("LEAO "+(i+1) +" INSTALADO COM SUCESSO");
 			}
-		}else {
-			System.out.println("QUANTIDADE DE ANIMAIS ULTRAPASSA A CAPACIDADE MAXIMA");
 		}
+		if(i<3) {
+			System.out.println("VOCE PODE ADICIONAR MAIS "+(3-i)+" LEAO(ES) À ESTA JAULA.");
+		}else{
+			System.out.println("CAPACIDADE MAXIMA ATINGIDA");
+		}
+		System.out.println("----------------------------------------------------------------------");
 	}
 	@SuppressWarnings("unused")
 	public static void criarInstalacaoTartaruga(Localizacao localizacao, Animal[] animal) {
-		int cont = 0 ;
-		for(Animal a : animal) {
-			cont++;
+		Tanque tanqueTartaruga = new Tanque();
+		tanqueTartaruga.setCapacidadeMaxima(17);
+		tanqueTartaruga.setLocalizacao(localizacao);
+		tanqueTartaruga.setTemperatura(20);
+		tanqueTartaruga.setVolumeEmLitros(200);
+		int i= 0;
+		for(i=0;i<animal.length;i++) {
+			boolean b = tanqueTartaruga.instalaAnimal(animal[i], i);
+			if(b) {
+				System.out.println("TARTARUGA "+(i+1) +" INSTALADA COM SUCESSO");
+			}
 		}
-		if(cont<=17) {
-			Tanque tanqueTartaruga = new Tanque(17, localizacao, 20, animal, 200);
-			if(cont<17) {
-				System.out.println("VOCE PODE ADICIONAR MAIS "+(17-cont)+" TARTARUGA(S) À ESTE TANQUE.");
-			}	
-		}else {
-			System.out.println("QUANTIDADE DE ANIMAIS ULTRAPASSA A CAPACIDADE MAXIMA");
+		if(i<17) {
+			System.out.println("VOCE PODE ADICIONAR MAIS "+(17-i)+" TARTARUGA(s) À ESTE TANQUE.");
+		}else{
+			System.out.println("CAPACIDADE MAXIMA ATINGIDA");
 		}
+		System.out.println("----------------------------------------------------------------------");
 	}
 	@SuppressWarnings("unused")
 	public static void criarInstalacaoPinguim(Localizacao localizacao, Animal[] animal) {
-		int cont = 0 ;
-		for(Animal a : animal) {
-			cont++;
-		}
-		if(cont<=25) {
-			Tanque tanquePinguim = new Tanque(25, localizacao, 15, animal, 100);
-			if(cont<25) {
-				System.out.println("VOCE PODE ADICIONAR MAIS "+(25-cont)+" PINGIM(S) À ESTE TANQUE.");
+		Tanque tanquePinguim = new Tanque();
+		tanquePinguim.setCapacidadeMaxima(25);
+		tanquePinguim.setLocalizacao(localizacao);
+		tanquePinguim.setTemperatura(15);
+		tanquePinguim.setVolumeEmLitros(100);
+		
+		int i= 0;
+		for(i=0;i<animal.length;i++) {
+			boolean b = tanquePinguim.instalaAnimal(animal[i], i);
+			if(b) {
+				System.out.println("PINGUIM "+(i+1) +" INSTALADO COM SUCESSO");
 			}
-		}else {
-			System.out.println("QUANTIDADE DE ANIMAIS ULTRAPASSA A CAPACIDADE MAXIMA");
 		}
+		if(i<25) {
+			System.out.println("VOCE PODE ADICIONAR MAIS "+(25-i)+" PINGUIM(S) À ESTE TANQUE.");
+		}else{
+			System.out.println("CAPACIDADE MAXIMA ATINGIDA");
+		}
+		System.out.println("----------------------------------------------------------------------");
 	}
 	@SuppressWarnings("unused")
 	public static void criarInstalacaoFoca(Localizacao localizacao, Animal[] animal) {
-		int cont = 0 ;
-		for(Animal a : animal) {
-			cont++;
-		}
-		if(cont<=5) {
-			Tanque tanqueFoca = new Tanque(5, localizacao, 15, animal, 100);
-			if(cont<5) {
-				System.out.println("VOCE PODE ADICIONAR MAIS "+(5-cont)+" FOCA(S) À ESTE TANQUE.");
+		Tanque tanqueFoca = new Tanque();
+		tanqueFoca.setCapacidadeMaxima(5);
+		tanqueFoca.setLocalizacao(localizacao);
+		tanqueFoca.setTemperatura(15);
+		tanqueFoca.setVolumeEmLitros(100);
+		
+		int i= 0;
+		for(i=0;i<animal.length;i++) {
+			boolean b = tanqueFoca.instalaAnimal(animal[i], i);
+			if(b) {
+				System.out.println("FOCA "+(i+1) +" INSTALADA COM SUCESSO");
 			}
-		}else {
-			System.out.println("QUANTIDADE DE ANIMAIS ULTRAPASSA A CAPACIDADE MAXIMA");
 		}
+		if(i<5) {
+			System.out.println("VOCE PODE ADICIONAR MAIS "+(5-i)+" FOCA(S) À ESTE TANQUE.");
+		}else{
+			System.out.println("CAPACIDADE MAXIMA ATINGIDA");
+		}
+		System.out.println("----------------------------------------------------------------------");
 	}
 	@SuppressWarnings("unused")
 	public static void criarInstalacaoPeixeBoi(Localizacao localizacao, Animal[] animal) {
-		int cont = 0 ;
-		for(Animal a : animal) {
-			cont++;
-		}
-		if(cont<=7) {
-			Tanque tanquePeixeBoi = new Tanque(7, localizacao, 18, animal, 750);
-			if(cont<7) {
-				System.out.println("VOCE PODE ADICIONAR MAIS "+(7-cont)+" PEIXE(S)-BOI À ESTE TANQUE.");
+		Tanque tanquePeixeBoi = new Tanque();
+		tanquePeixeBoi.setCapacidadeMaxima(7);
+		tanquePeixeBoi.setLocalizacao(localizacao);
+		tanquePeixeBoi.setTemperatura(18);
+		tanquePeixeBoi.setVolumeEmLitros(750);
+		
+		int i= 0;
+		for(i=0;i<animal.length;i++) {
+			boolean b = tanquePeixeBoi.instalaAnimal(animal[i], i);
+			if(b) {
+				System.out.println("PEIXE-BOI "+(i+1) +" INSTALADO COM SUCESSO");
 			}
-		}else {
-			System.out.println("QUANTIDADE DE ANIMAIS ULTRAPASSA A CAPACIDADE MAXIMA");
 		}
+		if(i<7) {
+			System.out.println("VOCE PODE ADICIONAR MAIS "+(7-i)+" PEIXE(S)-BOI À ESTE TANQUE.");
+		}else{
+			System.out.println("CAPACIDADE MAXIMA ATINGIDA");
+		}
+		System.out.println("----------------------------------------------------------------------");
 	}
 	@SuppressWarnings("unused")
 	public static void criarInstalacaoBaleia(Localizacao localizacao, Animal[] animal) {
-		int cont = 0 ;
-		for(Animal a : animal) {
-			cont++;
-		}
-		if(cont<=2) {
-			Tanque jaulaBaleia = new Tanque(2, localizacao, 19, animal, 1000);
-			if(cont<2) {
-				System.out.println("VOCE PODE ADICIONAR MAIS "+(2-cont)+" BALEIA À ESTE TANQUE.");
+		Tanque jaulaBaleia = new Tanque();
+		jaulaBaleia.setCapacidadeMaxima(2);
+		jaulaBaleia.setLocalizacao(localizacao);
+		jaulaBaleia.setTemperatura(19);
+		jaulaBaleia.setVolumeEmLitros(1000);
+		
+		int i= 0;
+		for(i=0;i<animal.length;i++) {
+			boolean b = jaulaBaleia.instalaAnimal(animal[i], i);
+			if(b) {
+				System.out.println("BALEIA "+(i+1) +" INSTALADA COM SUCESSO");
 			}
-		}else {
-			System.out.println("QUANTIDADE DE ANIMAIS ULTRAPASSA A CAPACIDADE MAXIMA");
 		}
+		if(i<2) {
+			System.out.println("VOCE PODE ADICIONAR MAIS "+(2-i)+" BALEIA À ESTE TANQUE.");
+		}else{
+			System.out.println("CAPACIDADE MAXIMA ATINGIDA");
+		}
+		System.out.println("----------------------------------------------------------------------");
 	}
 	@SuppressWarnings("unused")
 	public static void criarInstalacaoTubarao(Localizacao localizacao, Animal[] animal) {
-		int cont = 0 ;
-		for(Animal a : animal) {
-			cont++;
-		}
-		if(cont<=2) {
-			Tanque jaulaTubarao = new Tanque(2, localizacao, 22, animal, 500);
-			if(cont<2) {
-				System.out.println("VOCE PODE ADICIONAR MAIS "+(2-cont)+" TUBARAO À ESTE TANQUE.");
+		Tanque jaulaTubarao = new Tanque();
+		jaulaTubarao.setCapacidadeMaxima(2);
+		jaulaTubarao.setLocalizacao(localizacao);
+		jaulaTubarao.setTemperatura(22);
+		jaulaTubarao.setVolumeEmLitros(500);
+		
+		int i= 0;
+		for(i=0;i<animal.length;i++) {
+			boolean b = jaulaTubarao.instalaAnimal(animal[i], i);
+			if(b) {
+				System.out.println("TUBARAO "+(i+1) +" INSTALADO COM SUCESSO");
 			}
-		}else {
-			System.out.println("QUANTIDADE DE ANIMAIS ULTRAPASSA A CAPACIDADE MAXIMA");
 		}
+		if(i<2) {
+			System.out.println("VOCE PODE ADICIONAR MAIS "+(2-i)+" TUBARAO À ESTE TANQUE.");
+		}else{
+			System.out.println("CAPACIDADE MAXIMA ATINGIDA");
+		}
+		System.out.println("----------------------------------------------------------------------");
 	}
 }
